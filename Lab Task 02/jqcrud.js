37038757d55241d4550a9caffc6659193ed353c3
Mateen Haider <mateenwarraich2172@gmail.com>
2024-10-22 15:57:53 +0500

    lab

--- Lab Task 02/jqcrud.js
+++ Lab Task 02/jqcrud.js
@@ -33,7 +33,7 @@ function getData() {
 
 function getUserDataForSpecificUser(userId) {
   $.ajax({
-      url: "https://jsonplaceholder.typicode.com/users/${userId}",
+      url: `https://jsonplaceholder.typicode.com/users/${userId}`,
       method: "GET",
       dataType: "json",
       success: function (data) {
